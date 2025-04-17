4ecd3a3a6ba12af283e4825af08084eb046732f9
Eugene Wang <eugene.wang@greendotcorp.com>
2025-04-17 14:06:10 +0800

    test 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,13 +5,11 @@ resource "azurerm_frontdoor_firewall_policy" "example" {
   custom_rule {
     name     = var.rule_name
     priority = 1
-    rule_type = "MatchRule"
     action   = "Block"
 
     match_condition {
       match_variable = "RemoteAddr"
       operator       = "IPMatch"
-      negate_condition = false
       match_values   = [var.block_ip]
     }
   }
@@ -21,13 +19,11 @@ resource "azurerm_frontdoor_firewall_policy" "example" {
     content {
       name     = custom_rule.value.name
       priority = custom_rule.value.priority
-      rule_type = custom_rule.value.rule_type
       action   = custom_rule.value.action
 
       match_condition {
         match_variable = custom_rule.value.match_condition.match_variable
         operator       = custom_rule.value.match_condition.operator
-        negate_condition = custom_rule.value.match_condition.negate_condition
         match_values   = custom_rule.value.match_condition.match_values
       }
     }
